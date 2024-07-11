5d3c4f7cadc0617f046d7d40f170f1f784df9c2a
zoie peng <zoiepng3102@gmail.com>
2024-07-10 23:04:15 -0400

    move to client side and add logout functionality

---
+++ app/login.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Login() {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token: any = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        ?.split("=")[1];
+      console.log(token);
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  return (
+    <>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          login
+        </a>
+      ) : (
+        <button onClick={logout}>logout</button>
+      )}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-export default function Home() {
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const REDIRECT_URI = process.env.REDIRECT_URI;
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
+import Login from "./login";
 
+export default function Home() {
   return (
     <>
-      <a
-        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-      >
-        login to spotify
-      </a>
+      <Login />
     </>
   );
 }
