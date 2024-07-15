781061e365593fb0e09ce199432eb02ff12f71bf
zoie peng <zoiepng3102@gmail.com>
2024-07-15 13:19:24 -0400

    remove option to log in when already logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Login() {
+  const { status } = useSession();
+  // user already logged in
+  if (status == "authenticated") {
+    return (
+      <>
+        <p>ur alr logged in !!!!!!!</p>
+        <p>...why'd u come here</p>
+      </>
+    );
+  }
   return (
     <>
       <p>idk u</p>
