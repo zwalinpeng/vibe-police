1ec46c5eaac2f702af9b19111d4d915bf3ee17ae
zoie peng <zoiepng3102@gmail.com>
2024-07-15 13:18:57 -0400

    use status instead of session for log out button conditional render

--- app/components/logOutButt.tsx
+++ app/components/logOutButt.tsx
@@ -4,12 +4,12 @@ import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
 export default function LogOutButton() {
-  const { data: session } = useSession();
-  // don't render button is not signed in
-  if (!session) {
+  const { status } = useSession();
+  // don't render button is not logged in
+  if (status != "authenticated") {
     return <></>;
   }
-
+  // logged in, render logout button
   return (
     <>
       <button onClick={() => signOut()}>log out</button>
