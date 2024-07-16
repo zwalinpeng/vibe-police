f50b90587cc1fd16006e3716fa865f76392d56e4
zoie peng <zoiepng3102@gmail.com>
2024-07-15 22:42:43 -0400

    add page for playlist

---
+++ app/playlist/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getPlaylist } from "@/app/lib/spotify";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Playlist({ params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+  // redirect to login is not logged in
+  if (!session) {
+    redirect("/login");
+  }
+  const playlist = await getPlaylist({ session, id: params.id });
+  if (session.user.id != playlist.owner.id) {
+    return (
+      <>
+        <p>this not ur playlist...</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <p>{params.id}</p>
+    </>
+  );
+}
