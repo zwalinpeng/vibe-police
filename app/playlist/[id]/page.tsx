816979cea1c2cd57c17addc3fd93dcd1e5f94110
zoie peng <zoiepng3102@gmail.com>
2024-07-18 11:55:34 -0400

    display playlist stats

--- app/playlist/[id]/page.tsx
+++ app/playlist/[id]/page.tsx
@@ -1,7 +1,14 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getPlaylist } from "@/app/_lib/spotify";
+import {
+  getAllTracks,
+  getPlaylist,
+  getAudioFeatures,
+  getPlaylistStats,
+} from "@/app/_lib/spotify";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import StatViewer from "@/app/_components/statViewer";
 
 export default async function Playlist({ params }: { params: { id: string } }) {
   const session = await getServerSession(authOptions);
@@ -10,6 +17,7 @@ export default async function Playlist({ params }: { params: { id: string } }) {
     redirect("/login");
   }
   const playlist = await getPlaylist({ session, id: params.id });
+  // console.log(playlist);
   if (session.user.id != playlist.owner.id) {
     return (
       <>
@@ -17,9 +25,25 @@ export default async function Playlist({ params }: { params: { id: string } }) {
       </>
     );
   }
+  const thumbnail = playlist.images ? playlist.images[0].url : "/no_image.jpg";
+  const fields = "next,items(track(name, id))";
+  const tracks = await getAllTracks({ session, id: params.id, fields });
+  const track_ids: string[] = tracks.map((track: any) => track.id);
+  const track_features = await getAudioFeatures({ session, ids: track_ids });
+  const stats = getPlaylistStats({ track_features });
   return (
     <>
-      <p>{params.id}</p>
+      <div className="flex items-center">
+        <Image
+          src={thumbnail}
+          height={200}
+          width={200}
+          style={{ borderRadius: "5%", objectFit: "contain" }}
+          alt={`${playlist.name} thumbnail}`}
+        />
+        <p className="text-[200px]">{playlist.name}</p>
+      </div>
+      <StatViewer stats={stats} />
     </>
   );
 }
