8f3bbff1d157bce2d61927c8ccad0fb25a53db40
zoie peng <zoiepng3102@gmail.com>
2024-07-30 18:58:16 -0400

    change navbar logic

---
+++ app/_components/logButt.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
+
+export default function LogButton() {
+  const { status } = useSession();
+  const path = usePathname();
+  console.log(path);
+  // not logged in
+  if (status != "authenticated") {
+    // don't render button if on login page
+    if (path == "/login" || status == "loading") {
+      return;
+    }
+    return (
+      <>
+        <button onClick={() => signIn("spotify")}>log in</button>
+      </>
+    );
+  }
+  // logged in, render logout button
+  return (
+    <>
+      <button onClick={() => signOut()}>log out</button>
+    </>
+  );
+}
