a8cb6d25ea43f1d17e94579e8a3f163f9f4db64f
zoie peng <zoiepng3102@gmail.com>
2025-02-19 11:53:32 -0500

    bad practice... apostrophe + npm update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="mx-4 md:mx-36 mt-4 mb-12">
         <div className="mb-4">
           <h1 className="font-bold text-lg">
-            demo video, since api's got deprecated
+            demo video, since api&#39;s got deprecated
           </h1>
           <p>
             video is from before guest access was added, unfortunately I do not
